5fb4874bc9210cce6bd3d2572f34f8116223c6e5
prashanttanwar124 <prashanttanwar124@gmail.com>
2026-06-25 00:35:07 -0400

    Use ShouldBroadcast instead of ShouldBroadcastNow for QueueUpdated to avoid blocking requests

--- app/Events/QueueUpdated.php
+++ app/Events/QueueUpdated.php
@@ -6,11 +6,11 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class QueueUpdated implements ShouldBroadcastNow
+class QueueUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
